db1277e9d75b936faeb3e760cd886cec95d0d832
smartlegion <mysmartlegion@yandex.ru>
2021-08-30 17:08:59 +0700

    Added scroll to top

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,3 @@
-// Get that hamburger menu cookin' //
-
-document.addEventListener("DOMContentLoaded", function() {
-    // Get all "navbar-burger" elements
-    var $navbarBurgers = Array.prototype.slice.call(
-        document.querySelectorAll(".navbar-burger"),
-        0
-    );
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-        // Add a click event on each of them
-        $navbarBurgers.forEach(function($el) {
-            $el.addEventListener("click", function() {
-                // Get the target from the "data-target" attribute
-                var target = $el.dataset.target;
-                var $target = document.getElementById(target);
-                // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-                $el.classList.toggle("is-active");
-                $target.classList.toggle("is-active");
-            });
-        });
-    }
-});
-
 // Smooth Anchor Scrolling
 $(document).on("click", 'a[href^="#"]', function(event) {
     event.preventDefault();
@@ -46,12 +22,11 @@ function scrollFunction() {
     }
 }
 
-// Preloader
-$(document).ready(function($) {
-    $(".preloader-wrapper").fadeOut();
-    $("body").removeClass("preloader-site");
-});
-$(window).on("load", function() {
-    var Body = $("body");
-    Body.addClass("preloader-site");
+$(document).ready(function(){
+    // плавное перемещение страницы к нужному блоку
+    $("nav li a").click(function () {
+        elementClick = $(this).attr("href");
+        destination = $(elementClick).offset().top;
+        $("body,html").animate({scrollTop: destination }, 800);
+    });
 });
